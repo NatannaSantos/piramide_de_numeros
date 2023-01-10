2952f6ca7138d46dfa01e63068eb9a05d810291a
Natanna <natannacortez@gmail.com>
2023-01-10 14:46:58 -0300

    criando piramide

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        int interacoes = 5;
+        for(int i=1;i<=interacoes;i++){
+            for( int j=1;j<=i;j++){                
+                    System.out.print(i);                          
+            }
+            System.out.println( );
+        }
+    }
+}
